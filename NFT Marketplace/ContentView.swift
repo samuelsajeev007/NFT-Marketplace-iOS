53d9cd5ca8e90c81c775ce92ede5b49ac64d4de9
samuelsajeev007 <samuelsajeev6874@gmail.com>
2026-08-12 09:49:11 +0530

    Initial Commit

---
+++ NFT Marketplace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NFT Marketplace
+//
+//  Created by Samuel Sajeev on 12/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NFT Marketplace/NFT_MarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFT_MarketplaceApp.swift
+//  NFT Marketplace
+//
+//  Created by Samuel Sajeev on 12/08/26.
+//
+
+import SwiftUI
+
+@main
+struct NFT_MarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
